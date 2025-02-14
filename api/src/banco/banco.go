8ae75e439339b6a8d52d37379b632ba33ea58750
Alexander Ueno <alexander.ueno@edu.unifil.br>
2025-02-14 15:05:13 -0300

    Configurando db

---
+++ api/src/banco/banco.go
@@ -0,0 +1,23 @@
+package banco
+
+import (
+	"api/src/config"
+	"database/sql"
+
+	_ "guthub.com/go-sql-driver/mysql" // Driver
+)
+
+// SECTION - Conectar() abre a conexão com o banco de dados
+func Conectar() (*sql.DB, error) {
+	db, erro := sql.Open("mysql", config.StringConexaoBanco)
+	if erro != nil {
+		return nil, erro
+	}
+
+	if erro = db.Ping(); erro != nil {
+		db.Close()
+		return nil, erro
+	}
+
+	return db, nil
+}
--- api/src/config/config.go
+++ api/src/config/config.go
@@ -1,5 +1,14 @@
 package config
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
 var (
 	// NOTE - StringConexaoBanco é a string de conexão com o MySQL
 	StringConexaoBanco = ""
@@ -8,6 +17,21 @@ var (
 )
 
 // SECTION - Inicializar as variáveis de ambiente
-func Carregar(){
+func Carregar() {
+	var erro error
 
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal(erro)
+	}
+
+	Porta, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Porta = 9000
+	}
+
+	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USUARIO"),
+		os.Getenv("DB_SENHA"),
+		os.Getenv("DB_NOME"),
+	)
 }

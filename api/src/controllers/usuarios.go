9071cc3637f12ac7c7f481eee766990dece833a2
Alexander Ueno <alexander.ueno@edu.unifil.br>
2025-03-12 17:31:05 -0300

    buscar usuarios

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"strings"
 )
 
 // SECTION - Insere um usuário no banco de dados
@@ -48,7 +49,22 @@ func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 
 // SECTION - Busca os usuários no banco de dados
 func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Buscando todos os usuários!"))
+	nomeOuNick := strings.ToLower(r.URL.Query().Get("usuario"))
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuarios, erro := repositorio.Buscar(nomeOuNick)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	respostas.JSON(w, http.StatusOK, usuarios)
 }
 
 // SECTION - Busca um usuário no banco de dados
--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -3,6 +3,7 @@ package repositorios
 import (
 	"api/src/modelos"
 	"database/sql"
+	"fmt"
 )
 
 type usuarios struct {
@@ -19,20 +20,53 @@ func (repositorio usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
 	statement, erro := repositorio.db.Prepare(
 		"insert into usuarios (nome, nick, email, senha) values (?, ?, ?, ?)",
 	)
-	if erro != nil{
+	if erro != nil {
 		return 0, erro
 	}
 	defer statement.Close()
 
 	resultado, erro := statement.Exec(usuario.Nome, usuario.Nick, usuario.Email, usuario.Senha)
-	if erro != nil{
+	if erro != nil {
 		return 0, nil
 	}
 
 	ultimoIDInserido, erro := resultado.LastInsertId()
-	if erro != nil{
+	if erro != nil {
 		return 0, nil
 	}
 
 	return uint64(ultimoIDInserido), nil
+}
+
+// !SECTION - Buscar traz todos os usuários que atendem um filtro de nome ou nick
+func (repositorios usuarios) Buscar(nomeOuNick string) ([]modelos.Usuario, error) {
+	nomeOuNick = fmt.Sprintf("%%%s%%", nomeOuNick) // %nomeOuNick%
+
+	linhas, erro := repositorios.db.Query(
+		"select id, nome, nick, email, criadoEm from usuarios where nome LIKE ? or nick LIKE ?",
+		nomeOuNick, nomeOuNick,
+	)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var usuarios []modelos.Usuario
+
+	for linhas.Next() {
+		var usuario modelos.Usuario
+
+		if erro = linhas.Scan(
+			&usuario.ID,
+			&usuario.Nome,
+			&usuario.Nick,
+			&usuario.Email,
+			&usuario.CriadoEm,
+		); erro != nil {
+			return nil, erro
+		}
+
+		usuarios = append(usuarios, usuario)
+	}
+	return usuarios, nil
 }
